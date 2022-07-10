b156a4df3206bc9eb915c0a134507e8d86858f4b
kayembetshibangu <thierry.tshibangu@outlook.com>
2022-07-10 23:13:31 +0200

    added configuration to swagger.properties

---
+++ authentication/src/main/java/com/apphaircuts/mfcp/reboot/authentication/controller/AuthenticationControllerImpl.java
@@ -0,0 +1,10 @@
+package com.apphaircuts.mfcp.reboot.authentication.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AuthenticationControllerImpl {
+
+}
